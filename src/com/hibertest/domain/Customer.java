a33a4d4ab16e7d5f1b46ab99e3eadce1e5fc9fad
qiang54 <qiangzhaoo@163.com>
2018-03-08 20:52:36 +0800

    更新

--- src/com/hibertest/domain/Customer.java
+++ src/com/hibertest/domain/Customer.java
@@ -100,6 +100,13 @@ public class Customer {
 	public void setLinkmans(Set<Linkman> linkmans) {
 		this.linkmans = linkmans;
 	}
+	@Override
+	public String toString() {
+		return "Customer [cust_id=" + cust_id + ", cust_name=" + cust_name + ", cust_user_id=" + cust_user_id
+				+ ", cust_create_id=" + cust_create_id + ", cust_source=" + cust_source + ", cust_industry="
+				+ cust_industry + ", cust_level=" + cust_level + ", cust_linkman=" + cust_linkman + ", cust_phone="
+				+ cust_phone + ", cust_mobile=" + cust_mobile + "]";
+	}
 	
 	 
 	
